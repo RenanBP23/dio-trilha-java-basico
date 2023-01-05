ebf0d4892d41b8eeeba2688e74513395ad8095f6
rehsxx <renan.bpimentel23@gmail.com>
2023-01-05 08:50:15 -0800

    j

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
+import javax.lang.model.util.ElementScanner14;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int nota = 6;
+
+        if (nota < 6)
+            System.out.println("Reprovado");
+        else if (nota == 6)
+            System.out.println("Recuperacao");
+        else
+            System.out.println("Aprovado");
     }
 }
